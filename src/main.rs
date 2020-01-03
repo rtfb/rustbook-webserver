0a7d4fcd49eb23d8b93b6776fb6cff28d73bcccf
Vytautas Saltenis <vytas@rtfb.lt>
2020-01-03 19:13:06 +0200

    Respond with a hardcoded html

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::prelude::*;
+use std::fs;
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
@@ -13,4 +14,8 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
