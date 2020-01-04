64a6a1fa2b76131ee2b20fc9c090f95038615b71
Vytautas Saltenis <vytas@rtfb.lt>
2020-01-04 12:08:10 +0200

    Implement worker

--- src/lib.rs
+++ src/lib.rs
@@ -33,6 +33,8 @@ impl ThreadPool {
     pub fn execute<F>(&self, f: F)
         where
             F: FnOnce() + Send + 'static {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -43,8 +45,12 @@ struct Worker {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(|| {
-            receiver;
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+                println!("Worker {} got a job, executing", id);
+                job.call_box();
+            }
         });
         Worker{
             id,
@@ -53,4 +59,14 @@ impl Worker {
     }
 }
 
-struct Job;
+type Job = Box<dyn FnBox + Send + 'static>;
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)();
+    }
+}
