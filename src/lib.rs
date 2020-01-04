a9fa1aaca6eba1a3b70e574c44d32cb62d443221
Vytautas Saltenis <vytas@rtfb.lt>
2020-01-04 11:25:04 +0200

    Add Worker struct to hold the thread handle

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+use std::thread;
 
-pub struct ThreadPool;
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -11,11 +14,32 @@ impl ThreadPool {
     /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-        ThreadPool
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+        ThreadPool{
+            workers
+        }
     }
 
     pub fn execute<F>(&self, f: F)
         where
             F: FnOnce() + Send + 'static {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+        Worker{
+            id,
+            thread
+        }
     }
 }
